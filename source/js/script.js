9551469308949470d291f0abe75470d9c201ba1d
AlexeyRezanov <Alexey_Rezanov22@mail.ru>
2024-06-16 21:39:01 +0500

    Исправляет ошибки после первой проверки

--- source/js/script.js
+++ source/js/script.js
@@ -1,9 +1,14 @@
 let header = document.querySelector('.header');
 let nav = document.querySelector('.nav');
 let navToggle = document.querySelector('.nav__toggle');
+let locationImg = document.querySelector('.location__img');
+let locationMap = document.querySelector('.location__map');
 
 header.classList.remove('header--nojs');
 nav.classList.remove('nav--nojs');
+locationImg.classList.remove('location__img--nojs');
+locationMap.classList.remove('location__map--nojs');
+
 
 navToggle.addEventListener('click', function () {
   if (nav.classList.contains('nav--closed')) {
